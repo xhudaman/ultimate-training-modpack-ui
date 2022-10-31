6495d108a49456b47d31954bf70aa8479125cb78
xhudaman <edell.matthew@gmail.com>
2022-10-31 04:30:57 -0400

    Removed autofocus attr

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -268,7 +268,6 @@ const App = () => {
                       }}
                       handleFocus={() => setHelpText(helpText)}
                       handleMouseEnter={() => setHelpText(helpText)}
-                      autofocus
                     >
                       <div className="flex justify-start items-center w-full h-full">
                         <img
